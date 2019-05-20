a11a1871865142cf9e3621ef94114cd00abf89c7
Jonas Klimes <jonas.klimes@profinit.eu>
2019-05-20 17:47:34 +0200

    Remove script

--- vars/runUnitTests.groovy
+++ vars/runUnitTests.groovy
@@ -1,9 +1,7 @@
 def call() {
-    script {
-        try {
-            sh "mvn test -Dmaven.test.failure.ignore=true"
-        } finally {
-            junit '**/target/surefire-reports/*.xml'
-        }
+    try {
+        sh "mvn test -Dmaven.test.failure.ignore=true"
+    } finally {
+        junit '**/target/surefire-reports/*.xml'
     }
 }
