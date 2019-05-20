94674ed9b05147b42a47ab46a25185aa49f8dfd5
Jonas Klimes <jonas.klimes@profinit.eu>
2019-05-20 18:10:26 +0200

    Refactor

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,7 +1,8 @@
 import mypackage.Constants;
 
 def call(relativeUrl, branch) {
+    def baseUrl = Constants.SCM_BASE_URL
     checkout([$class: 'GitSCM', branches: [[name: '*/master']],
               doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
-              userRemoteConfigs: [[url: "${Constants.SCM_BASE_URL}${relativeUrl}"]]])
+              userRemoteConfigs: [[url: "${baseUrl}${relativeUrl}"]]])
 }
