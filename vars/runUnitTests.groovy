77606200ec352cc2de737ebaec2f58e0d0b9021c
Jonas Klimes <jonas.klimes@profinit.eu>
2019-05-20 17:41:37 +0200

    remove step

--- vars/runUnitTests.groovy
+++ vars/runUnitTests.groovy
@@ -1,9 +1,7 @@
 def call() {
-    step {
-        try {
-            sh "mvn test -Dmaven.test.failure.ignore=true"
-        } finally {
-            junit '**/target/surefire-reports/*.xml'
-        }
+    try {
+        sh "mvn test -Dmaven.test.failure.ignore=true"
+    } finally {
+        junit '**/target/surefire-reports/*.xml'
     }
 }
