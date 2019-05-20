4cc35fef1ee7eae097f662ce7232060739bb2ddb
Jonas Klimes <jonas.klimes@profinit.eu>
2019-05-20 18:19:30 +0200

    Fix args

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,6 +1,7 @@
 import mypackage.Constants;
 
-def call(relativeUrl) {
+def call(args) {
+    def relativeUrl = args.relativeUrl
     def baseUrl = Constants.SCM_BASE_URL
     checkout([$class: 'GitSCM', branches: [[name: '*/master']],
               doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
