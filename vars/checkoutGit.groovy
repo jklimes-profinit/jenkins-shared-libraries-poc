a029871195b6e95d8fd8ed07f574e36e55f21d9c
Jonas Klimes <jonas.klimes@profinit.eu>
2019-05-20 18:14:30 +0200

    Test something

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -2,7 +2,5 @@ import mypackage.Constants;
 
 def call(relativeUrl, branch) {
     def baseUrl = Constants.SCM_BASE_URL
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-              doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
-              userRemoteConfigs: [[url: "${baseUrl}${relativeUrl}"]]])
+    sh "echo Base URL: ${baseUrl}"
 }
