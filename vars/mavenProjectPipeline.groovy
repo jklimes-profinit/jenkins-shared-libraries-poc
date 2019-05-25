d03084e233a9135a55c9619f8c668e573fb9c431
Jonas Klimes <jonas.klimes@profinit.eu>
2019-05-25 17:54:36 +0200

    Skip tests param

--- vars/mavenProjectPipeline.groovy
+++ vars/mavenProjectPipeline.groovy
@@ -1,52 +1,53 @@
-def call(scmUrl, additionalRecipients ) {
-      pipeline {
-            agent any
-            tools {
-                  maven 'Maven'
+def call(scmUrl, additionalRecipients) {
+    pipeline {
+        agent any
+        tools {
+            maven 'Maven'
+        }
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '5'))
+            disableConcurrentBuilds()
+        }
+        triggers {
+            pollSCM 'H/2 * * * *'
+        }
+        parameters {
+            booleanParam name: 'SKIP_TESTS', defaultValue: false, description: 'Whether to skip tests'
+        }
+        stages {
+            stage("Configure") {
+                steps {
+                    script {
+                        configure()
+                    }
+                }
             }
-            options {
-                  buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '5'))
-                  disableConcurrentBuilds()
+            stage('Checkout') {
+                steps {
+                    checkoutGit(relativeUrl: scmUrl, branch: 'master')
+                }
             }
-            triggers {
-                  pollSCM 'H/2 * * * *'
+            stage('Build') {
+                steps {
+                    mavenBuild()
+                }
             }
-            parameters {
-                  booleanParam name: 'SKIP_TESTS', defaultValue: false, description: 'Whether to skip tests'
+            stage('Test') {
+                when { expression { return !params.SKIP_TESTS } }
+                steps {
+                    runUnitTests()
+                }
             }
-            stages {
-                  stage ("Configure"){
-                        steps {
-                              script {
-                                    configure()
-                              }
-                        }
-                  }
-                  stage('Checkout') {
-                        steps {
-                              checkoutGit(relativeUrl: scmUrl, branch: 'master')
-                        }
-                  }
-                  stage('Build') {
-                        steps {
-                              mavenBuild()
-                        }
-                  }
-                  stage('Test') {
-                        steps {
-                              runUnitTests()
-                        }
-                  }
+        }
+        post {
+            always {
+                script {
+                    fixBuildResult
+                    sendEmailNotification(additionalRecipients: additionalRecipients)
+                }
             }
-            post {
-                  always {
-                        script {
-                              fixBuildResult
-                              sendEmailNotification(additionalRecipients: additionalRecipients)
-                        }
-                  }
 
-            }
-      }
+        }
+    }
 
 }
